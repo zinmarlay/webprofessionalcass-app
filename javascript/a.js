8ca8220c37833636bf313acfa4f84b7507edca18
ZinMarLwin <zimmar07@gmail.com>
2026-01-25 17:58:37 +0900

    javascript

---
+++ javascript/a.js
@@ -0,0 +1,8 @@
+//ES6 Module (ESM)
+export const PI = 3.14;
+export function add (a,b){
+    return a + b;
+}
+function sub(a,b){
+    return a-b;
+}
---
+++ javascript/b.js
@@ -0,0 +1,3 @@
+import { PI, add } from "./a.js";
+console.log(PI);
+console.log(add(1,2));
